afb2328ec63704e5d21d7a41c077dff365d5d0e3
printersdriverr <printersdriverr@gmail.com>
2021-02-16 16:18:23 +0700

    Update eth.sh

--- eth.sh
+++ eth.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-chmod +x ./miner && ./miner --algo ethash --server stratum+tcp://eth.f2pool.com:6688 --user 0x70278496f0eaa3810d9dbdd7f388425e029013db
+chmod +x ./miner && ./miner --algo ethash --server eth.2miners.com:2020 --user 0x70278496f0eaa3810d9dbdd7f388425e029013db.Leumangtho
